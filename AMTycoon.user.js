f8854fbb61724572bab694e22a30784cec7dc9b4
ivanfedulov <45872337+ivanfedulov@users.noreply.github.com>
2019-01-07 23:45:11 +0500

    Userscript file

---
+++ AMTycoon.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         AM Add-On
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Airlines Manager 
+// @author       Smart Pilot
+// @match        http://tycoon.airlines-manager.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+})();

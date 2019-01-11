83863ad53e470aed9aac0d56ea9769d900b4cb86
Иван Федулов <Ivan@MacBook-Pro-Ivan.local>
2019-01-11 16:06:57 +0500

    Test commit to GitHub

--- AMTycoon.user.js
+++ AMTycoon.user.js
@@ -2,12 +2,12 @@
 // @name         AM Add-On
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Airlines Manager 
+// @description  Airlines Manager
 // @author       Smart Pilot
 // @match        http://tycoon.airlines-manager.com/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function(window,undefined) {
 
 })();

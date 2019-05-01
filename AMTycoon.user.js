003b9617735be3854467244c91094b4461f1bae4
IVAN FEDULOV <45872337+ivanfedulov@users.noreply.github.com>
2019-05-01 16:28:29 +0500

    Update AMTycoon.user.js

--- AMTycoon.user.js
+++ AMTycoon.user.js
@@ -1,18 +1,54 @@
 // ==UserScript==
 // @name         AM Add-On
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.1
 // @description  Airlines Manager
 // @author       Smart Pilot
-// @match        https://tycoon.airlines-manager.com/*
+// @match        https://tycoon.airlines-manager.com/aircraft
 // @grant        none
+// @include      https://apis.google.com/js/api.js
 // ==/UserScript==
 
-function Idle() {
-  setTimeout(Idle, 5000);
-
-}
 
 (function(window,undefined) {
-  Idle();
+  var divRename = $("<div/>", {
+      id: "divRename",
+      css: {
+          "margin-bottom" : "10px"
+      }
+  }).insertBefore($("#hubDropdown").parent());
+  var divTitle = $("<div/>", {
+      id: "divRenameTitle",
+      css: {
+          "margin-bottom" : "10px"
+      }
+  }).appendTo(divRename);
+  divTitle.html("<b>RENAME AIRCRAFTS:</b>");
+  $("<span/>").appendTo(divRename).html("Prefix:");
+  var inputPrefix = $("<input/>", {
+      type: "text",
+      id: "iRenPrefix",
+      css: {
+          "margin-left"  : "5px",
+          "margin-right" : "20px"
+      }
+  }).appendTo(divRename);
+  $("<span/>").appendTo(divRename).html("Pattern:");
+  var inputPattern = $("<input/>", {
+      type: "text",
+      id: "iRenPattern",
+      css: {
+        "margin-left" : "5px"
+      }
+  }).appendTo(divRename);
+  var buttonRename = $("<input/>", {
+      type: "button",
+      value: "RENAME",
+      id: "iRenButtonRename",
+      css: {
+        "margin-left" : "10px"
+      }
+  }).appendTo(divRename);
+
+
 })();
